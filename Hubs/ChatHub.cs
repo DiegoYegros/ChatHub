d70ab61af7c32d5c3b7ba8dfe8eedbd9aea7608c
Diego Yegros <diegoyegrosa@gmail.com>
2023-10-21 14:19:12 -0300

    WIP

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -6,6 +6,9 @@ public class ChatHub : Hub
 {
     private readonly string _botUser;
     private readonly IDictionary<string, UserConnection> _connections;
+
+    private const string LOBBY_GROUP_NAME = "LobbyGroup";
+
     public ChatHub(IDictionary<string, UserConnection> connections)
     {
         _botUser = "Almighty";
@@ -20,24 +23,40 @@ public class ChatHub : Hub
             .SendAsync("ReceiveMessage", userConnection.User + " " + DateTime.Now.ToString("HH:mm"), message);
         }
     }
+    public override async Task OnConnectedAsync()
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, LOBBY_GROUP_NAME);
+        var rooms = _connections.Values.GroupBy(c => c.Room).ToList();
+        await base.OnConnectedAsync();
+        await Clients.Group(LOBBY_GROUP_NAME).SendAsync("RoomsAndAmountOfPeople", rooms);
+    }
 
     public override Task OnDisconnectedAsync(Exception exception)
     {
         if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
         {
             _connections.Remove(Context.ConnectionId);
+            Groups.RemoveFromGroupAsync(Context.ConnectionId, LOBBY_GROUP_NAME);
             Clients.Group(userConnection.Room)
             .SendAsync("ReceiveMessage", _botUser + " " + DateTime.Now.ToString("HH:mm"), $"{userConnection.User} has left");
             SendConnectedUsers(userConnection.Room);
         }
-        return base.OnDisconnectedAsync(exception);
+        base.OnDisconnectedAsync(exception);
+        return UpdateClientsInLobbyWithRoomList();
     }
     public async Task JoinRoom(UserConnection userConnection)
     {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, LOBBY_GROUP_NAME);
         await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
         _connections[Context.ConnectionId] = userConnection;
         await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser + " " + DateTime.Now.ToString("HH:mm"), $"{userConnection.User} has joined");
         await SendConnectedUsers(userConnection.Room);
+        await UpdateClientsInLobbyWithRoomList();
+    }
+    public async Task UpdateClientsInLobbyWithRoomList()
+    {
+        var rooms = _connections.Values.GroupBy(c => c.Room).ToList();
+        await Clients.Group(LOBBY_GROUP_NAME).SendAsync("RoomsAndAmountOfPeople", rooms);
     }
     public Task SendConnectedUsers(string room)
     {
@@ -46,4 +65,5 @@ public class ChatHub : Hub
         .Select(c => c.User);
         return Clients.Group(room).SendAsync("UsersInRoom", users);
     }
+
 }
